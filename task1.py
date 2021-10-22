5eaef90177b1d961b43f1ac0c35c7c40218a0dba
dgrishanovich <dariushka14@mail.ru>
2021-10-22 15:22:51 +0300

    clear master branch

--- task1.py
+++
@@ -1,9 +0,0 @@
-firstString = 'Hello world!'
-print(firstString)
-
-name = input('What is your name?')
-print('Name is ', name)
-
-age = int(input('How old are you?'))
-
-print('Age = ', age)
--- task3.py
+++
@@ -1,3 +0,0 @@
-initialNumber = int(input('Type initial number: '))
-resultNumber = 3*initialNumber + 20*initialNumber + 100*initialNumber
-print(resultNumber)
