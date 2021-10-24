e94ec6d5dc9c59a988162234fa823e7870ab1419
dgrishanovich <dariushka14@mail.ru>
2021-10-24 20:55:03 +0300

    Changes in format for task2

--- task2.py
+++ task2.py
@@ -1,10 +1,6 @@
-import datetime
-
 timeSec = int(input('Input time in seconds: '))
-t = str(datetime.timedelta(seconds=timeSec))
-print(t)
 
-#hours = timeSec // 3600
-#minutes = timeSec // 60 - hours * 60
-#seconds = timeSec % 60
-#print('Time is: ' + str(hours) + ':' + str(minutes) + ':' + str(seconds))
+hours = timeSec // 3600
+minutes = timeSec // 60 - hours * 60
+seconds = timeSec % 60
+print('{:02}:{:02}:{:02}'.format(hours, minutes, seconds))
