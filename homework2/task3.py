9a0c8b34e966207fe6728b56bf00cff7f2560280
dgrishanovich <dariushka14@mail.ru>
2021-10-26 13:50:09 +0300

    task3 updated

--- homework2/task3.py
+++ homework2/task3.py
@@ -3,12 +3,14 @@ monthsList = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
 inputMonthNumber = int(input("Input month number: "))
 strMonth = monthsList[inputMonthNumber - 1]
 
-solutionType = input('PLease choose the type of solution: List - 0, Dict - 1: ')
+solutionType = int(input('PLease choose the type of solution: List - 0, Dict - 1: '))
 if solutionType == 0: # List Solution
+    print('List solution')
     monthsListMapping = ['Winter', 'Winter', 'Spring', 'Spring', 'Spring', 'Summer', 'Summer', 'Summer',
                          'Autumn', 'Autumn', 'Autumn', 'Winter']
     print(monthsListMapping[inputMonthNumber - 1])
-else: # Dict Solution
+elif solutionType == 1: # Dict Solution
+    print('Dict solution')
     monthSeasonDict = {'Winter': ['January', 'February', 'December'], 'Spring': ['March', 'April', 'May'],
                        'Summer': ['June', 'July', 'August'], 'Autumn': ['September', 'October', 'November']}
     for key, value in monthSeasonDict.items():
