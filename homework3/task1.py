9dceaa0046ba2eb300153133774de61d43201ed8
dgrishanovich <dariushka14@mail.ru>
2021-11-02 22:22:08 +0300

    Removed homework3 tasks from homework#4

--- homework3/task1.py
+++
@@ -1,9 +0,0 @@
-def division(a, b):
-    try:
-        return a / b
-    except ZeroDivisionError:
-        return 'Division by 0!'
-
-
-numerator, denominator = [float(x) for x in input('Input a, b: ').split()]
-print(division(numerator, denominator))
